4b45df00befae6a4fb38e9c055ff437820c80d60
Juha Ruotsalainen <juharu@ambwks606.torppa.lan>
2021-10-26 20:53:58 +0300

    Initial commit.

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="row">
+    <div class="col-2 offset-2">
+      <q-btn @click="count++" color="primary">count is: {{ count }}</q-btn>
+    </div>
+    <div class="col-2 offset-4">
+      <q-btn @click="count++" color="secondary">count is: {{ count }}</q-btn>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col offset-4">
+      <DatePicker />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import DatePicker from './DatePicker.vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
